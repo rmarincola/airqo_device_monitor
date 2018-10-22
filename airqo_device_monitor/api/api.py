2e68d25996300353d7f2d62dccea56901260e81b
Rachel <rmarincola@gmail.com>
2018-10-22 14:18:47 +0300

    remove folder.

--- airqo_device_monitor/api/api.py
+++
@@ -1,9 +0,0 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
-
-class MalfunctioningChannels(BaseHTTPRequestHandler):
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header('Content-type', 'text/json')
-		self.end_headers()
-		self.wfile.write("{'hi': 'hello'}")
